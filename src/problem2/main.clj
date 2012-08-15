01193ae99cb41e624cd28f5ba0c6c8b6a969e9bc
Mathias Birkkjær Nielsen <mathiasbn@gmail.com>
2012-08-15 22:49:02 +0200

    Function for computing list of fibs of length x. Function for finding fib x.

--- src/problem2/main.clj
+++ src/problem2/main.clj
@@ -1,3 +1,9 @@
 (ns problem2.main)
 
+(def fibListGenerator (iterate (fn [n] (cons (+ (first n) (second n)) n)) [2 1]))
+(def fibListOf (fn [n] (nth fibListGenerator (- n 2))))
+(def fibNum (fn [n] (first (fibListOf n))))
+
+(println (fibListOf 32))
+(println (concat "fib nr 32 is less then 4 mill:" (str (< (fibNum 32) 4000000N))))
 
